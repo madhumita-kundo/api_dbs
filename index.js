6b00998e5b8b745fd6d81d3112bd3923ebc11e97
madhumita <32956360+madhumita-kundo@users.noreply.github.com>
2018-08-09 21:14:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/', function(req, res) {
-    res.redirect('/index.html');
+    res.redirect('/public/index.html');
 });
 app.listen(process.env.PORT || 3000);
